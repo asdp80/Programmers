b4aac6cd2f7b4e67f4b58db8742c3d3f6f317687
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-16 10:13:08 +0900

    [level 0] Title: 공배수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.py
+++ 프로그래머스/0/181936. 공배수/공배수.py
@@ -1,8 +1,14 @@
-def solution(number, n, m):
+"""def solution(number, n, m):
     if number % n == 0:
         if number % m == 0:
             return 1
         else: 
             return 0
     else :
-        return 0
+        return 0"""
+
+def solution(number, n, m):
+    if number % n == 0 and number % m == 0:
+        return 1
+
+    return 0
