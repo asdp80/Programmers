f78e45175c70d72e006a483dc6dde3e6323da8a1
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-19 14:54:01 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,7 +1,5 @@
 def solution(money):
-    answer = []
-    cnt , remain = money // 5500, money % 5500
-    answer.append(cnt)
-    answer.append(remain)
+    answer = [money // 5500, money % 5500]
     
+
     return answer
