194de2a96d9cbb26671756e75cec2a8421ca750e
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-22 17:59:35 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -0,0 +1,21 @@
+"""
+1. 배열 순회
+2. 전 인덱스와 다음 인덱스의 값 비교
+3. 큰 수 변수에 저장, 인덱스도 다른 변수에 저장
+4. 출력
+"""
+
+def solution(array):
+    a, b = 0, 0
+    answer = []
+    for i in range(0, len(array)-1) :
+        if array[i] > array[i+1] :
+            a = array[i]
+            b = i
+        else :
+            a = array[i+1]
+            b = i+1
+    answer.append(a)
+    answer.append(b)
+    
+    return answer
