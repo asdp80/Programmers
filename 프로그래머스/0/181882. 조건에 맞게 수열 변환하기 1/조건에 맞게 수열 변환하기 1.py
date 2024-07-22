08811551cee01db05fba88a7495250885c0fcab9
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-23 08:08:17 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 117.28 ms, Memory: 48.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -0,0 +1,23 @@
+"""
+1-1. 50보다 크거나 같은지 확인
+1-2. 짝수인지 확인 후 맞으면 2로 나눔
+2-1. 1-1d의 elif 작으면 : 
+2-2. 홀수이면 * 2
+3. else : 그냥 출력
+"""
+def solution(arr):
+    answer = []
+    for i in arr :
+        if i >= 50 :
+            if i % 2 == 0 :
+                answer.append(i/2)
+            else : answer.append(i)
+        elif i < 50 :
+            if i % 2 != 0 :
+                answer.append(i*2)
+            else : 
+                answer.append(i)
+        else :
+            answer.append(i)
+                
+    return answer
