efd43fda5c77f8db4a1743d2cb8f49d9e753ac98
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-22 22:24:54 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -0,0 +1,12 @@
+"""
+1. 문자열 배열로 바꾸기
+2. 인덱스 두개 스왑
+3. 배열 문자열로 변환
+4. 출력
+"""
+
+def solution(my_string, num1, num2):
+    answer = list(my_string)
+    answer[num1], answer[num2] = answer[num2], answer[num1]
+    answer = ''.join(answer)
+    return answer
