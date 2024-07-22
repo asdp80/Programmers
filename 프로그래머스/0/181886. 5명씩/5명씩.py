b36483fe6f6bfc8a3ced1e6d750a29984050359b
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-23 08:31:57 +0900

    [level 0] Title: 5명씩, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.py
+++ 프로그래머스/0/181886. 5명씩/5명씩.py
@@ -4,3 +4,11 @@ def solution(names):
         if (names.index(i)+1) % 5 == 1 :
             answer.append(i)
     return answer
+
+"""
+def solution(names):
+    answer = []
+    for i in range(0,len(names),5):
+        answer.append(names[i])
+    return answer
+"""
