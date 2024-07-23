5d654c7618e59d570ea9d26cf2d08d28f7628a13
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-23 13:07:39 +0900

    [level 0] Title: 카운트 업, Time: 0.01 ms, Memory: 9.94 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.py
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.py
@@ -3,3 +3,8 @@ def solution(start_num, end_num):
     for i in range(start_num, end_num + 1) :
         answer.append(i)
     return answer
+
+"""
+def solution(start, end):
+    return [i for i in range(start,end+1)]
+"""
