1c865b64d1a69795ef1b57c0b53b7c9c50aba544
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-23 15:58:35 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    answer = 0
+    numbers.sort()
+    if numbers[-1] * numbers[-2] > numbers[0] * numbers[1] : 
+        answer = numbers[-1] * numbers[-2]
+    else : 
+        answer = numbers[0] * numbers[1]
+    return answer
