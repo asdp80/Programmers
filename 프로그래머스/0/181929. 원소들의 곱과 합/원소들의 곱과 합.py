5a07710c29479af738860b01096f517dbe92c79e
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-23 12:27:01 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -5,3 +5,11 @@ def solution(num_list):
         answer *= i
         
     return 1 if answer < a else 0
+
+"""
+def solution(num_list):
+    mul = 1 
+    for n in num_list:
+        mul *= n
+    return int(mul < sum(num_list) ** 2)
+"""
