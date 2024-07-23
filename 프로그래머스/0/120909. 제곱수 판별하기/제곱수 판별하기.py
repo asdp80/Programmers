ed3e83fa44c465fbf7b534a08623353ee2dcba44
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-23 15:47:27 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -2,3 +2,11 @@ import math
 def solution(n):
     answer = math.sqrt(n)
     return 1 if answer == int(answer) else 2
+
+"""
+def solution(n):
+    for i in range(1,n):
+        if n == i * i:
+            return 1
+    return 2
+"""
