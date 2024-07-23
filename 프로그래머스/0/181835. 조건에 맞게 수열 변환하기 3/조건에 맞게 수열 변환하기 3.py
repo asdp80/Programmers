a041132164ebd55a56f89389442e6cff28464703
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-23 13:16:13 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 78.80 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
@@ -1,8 +1,11 @@
-def solution(arr, k):
+"""def solution(arr, k):
     answer = []
     for i in arr : 
         if k % 2 == 0 : 
             answer.append(i + k)
         else : 
             answer.append(i * k)
-    return answer
+    return answer"""
+
+def solution(arr, k):
+    return [i*k if k%2!=0 else i+k for i in arr]
