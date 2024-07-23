a375829a7d5017ba3ac25467bfe588fbe853234d
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-23 13:18:43 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
@@ -6,3 +6,8 @@ def solution(str_list, ex):
         else :
             answer += ''
     return answer
+
+"""
+def solution(str_list, ex):
+    return ''.join([i for i in str_list if ex not in i])
+"""
