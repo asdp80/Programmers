1dae2a6375db611d98d3d4c7b7f181105f6dcd89
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-23 16:03:48 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.py
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.py
@@ -3,3 +3,11 @@ def solution(strlist):
     for i in range(len(strlist)) :
         answer.append(len(strlist[i]))
     return answer
+
+"""
+def solution(strlist):
+    answer =[]
+    for i in strlist:
+        answer.append(len(i))
+    return answer
+"""
