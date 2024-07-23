25601187677925b59bc0967df74403b5b646dd02
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-23 12:03:42 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.12 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.py
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.py
@@ -5,3 +5,11 @@ def solution(my_string, index_list):
         
     answer = ''.join(answer)
     return answer
+
+"""
+def solution(my_string, index_list):
+    answer = ""
+    for i in index_list:
+        answer += my_string[i]
+    return answer
+"""
