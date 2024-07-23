893af3cee0e6fe36e6c36ad6ee67ae64f97d75a9
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-23 15:52:20 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -0,0 +1,8 @@
+def solution(my_string, letter):
+    answer = ''
+    for i in range(0, len(my_string)) :
+        if my_string[i] != letter :
+            answer += my_string[i]
+        else :
+            answer += ""
+    return answer
