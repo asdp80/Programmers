a3a6a16b87818c68871b0ec4fdc48f55ee9b058a
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-23 18:11:06 +0900

    [level 0] Title: 가위 바위 보, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -8,3 +8,9 @@ def solution(rsp):
         else :
             answer += "2"
     return answer
+
+"""
+def solution(rsp):
+    d = {'0':'5','2':'0','5':'2'}
+    return ''.join(d[i] for i in rsp)
+"""
