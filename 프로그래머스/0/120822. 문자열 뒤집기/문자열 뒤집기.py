6d2f866a0a01187b37e93b9e7e775a1e6f3d8f8b
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-23 15:37:41 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
@@ -3,3 +3,8 @@ def solution(my_string):
     for i in range(1, len(my_string)+1):
         answer += my_string[-i]
     return answer
+
+"""
+def solution(my_string):
+    return my_string[::-1]
+"""
