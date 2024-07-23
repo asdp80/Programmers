05ef60a7e69bb09974d1945bf804583e0df7e708
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-23 11:49:37 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -1,3 +1,9 @@
 def solution(my_string, is_suffix):
     answer = 0
     return 1 if my_string.endswith(is_suffix) else 0
+
+"""
+def solution(m, s):
+    if m[-len(s):]==s: return 1
+    return 0
+"""
