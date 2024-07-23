590646b07806bcac466ee0685c821744dd7aee3b
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-23 17:46:24 +0900

    [level 0] Title: 369게임, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.py
+++ 프로그래머스/0/120891. 369게임/369게임.py
@@ -5,3 +5,12 @@ def solution(order):
         if int(i) == 3 or int(i) == 6 or int(i) == 9 :
             count += 1
     return count
+
+"""
+def solution(order):
+    answer = 0
+    for i in str(order):
+        if i in ["3", "6", "9"]:
+            answer += 1
+    return answer
+"""
