ca39d3ee2ba69a122e74b83b0a175d0cf98d59b5
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-23 12:09:47 +0900

    [level 0] Title: 수 조작하기 1, Time: 15.22 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,13 +1,16 @@
 def solution(n, control):
-    answer = 0
     for i in control :
-        if i == "w" : 
-            n += 1
-        elif i == "s" :
-            n -= 1
-        elif i == "d" :
-            n += 10
-        else :
-            n -= 10
-            
+        if i == "w" : n += 1
+        elif i == "s" : n -= 1
+        elif i == "d" : n += 10
+        else : n -= 10       
     return n
+
+"""
+def solution(n, control):
+    answer = n
+    c = { 'w':1, 's':-1, 'd':10, 'a':-10}
+    for i in control:
+        answer += c[i]
+    return answer
+"""
