b5803ad90770e24c085b2227b173992c1750b8f4
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-24 22:20:16 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 1020.77 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -7,3 +7,11 @@ def solution(a, b):
         for i in range(b, a+1) :
             answer += i
     return answer
+
+"""
+def adder(a, b):
+    return sum(range(min(a, b), max(a, b)+1))
+
+def solution(a, b):
+    return sum(range(a,b+1) if a <= b else range(b,a+1))
+"""
