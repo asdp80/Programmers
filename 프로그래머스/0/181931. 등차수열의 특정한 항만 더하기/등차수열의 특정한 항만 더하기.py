df928cc3b7a8aa4363e27361022bfb8a0490e385
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-24 14:44:43 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -0,0 +1,9 @@
+def solution(a, d, included):
+    answer = a
+    result = 0
+    for i in range(len(included)) :
+        if included[i] == 1 :
+            result += answer
+        answer += d
+            
+    return result
