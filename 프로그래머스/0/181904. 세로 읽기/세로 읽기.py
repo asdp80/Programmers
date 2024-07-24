8eb8489179e2a422abbb4c45df3a84c0e67c8c7f
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-24 14:31:15 +0900

    [level 0] Title: 세로 읽기, Time: 0.27 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -12,3 +12,17 @@ def solution(my_string, m, c):
         result += answer[i][c-1]
     
     return result
+"""
+def solution(my_string, m, c):
+    answer = ''
+    
+    # 문자열을 m 글자씩 나누어 2차원 배열로 변환
+    for i in range(0, len(my_string), m):
+        answer += my_string[i:i+m][c-1]
+    
+    return answer
+"""
+"""
+def solution(s, m, c):
+    return s[c-1::m]
+"""
