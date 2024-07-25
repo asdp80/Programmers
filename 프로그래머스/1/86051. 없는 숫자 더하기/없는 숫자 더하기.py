106555ddbc73956425afe9699d9dc42ef11c04a1
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-25 09:41:28 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,3 +1,12 @@
 def solution(numbers):
     
     return 45 - sum(numbers)
+
+"""
+def solution(numbers):
+    answer = 0
+    for i in range(1, 10) : 
+        if i not in numbers :
+            answer += i
+    return answer
+"""
