71685a840c6ebf83cf4d983db3864daf8542dbc0
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-26 19:27:21 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -21,6 +21,10 @@ def solution(emergency):
         arr.append(idx)
     return arr
 """
-
+"""
+def solution(emergency):
+    sort = sorted(emergency, reverse=True)
+    return [sort.index(i) + 1 for i in emergency]
+"""
 
 
