5fa05df5fb766bb89ae28f6e42f5b41ee4eed24c
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-26 15:09:27 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 3.69 ms, Memory: 13.3 MB -BaekjoonHub

--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
@@ -7,3 +7,10 @@ def solution(arr, divisor):
         answer.append(-1)
     
     return sorted(answer)
+
+"""
+def solution(arr, divisor):
+    arr = [x for x in arr if x % divisor == 0];
+    arr.sort();
+    return arr if len(arr) != 0 else [-1];
+"""
