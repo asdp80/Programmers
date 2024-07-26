5b4ac551da40e5a923d72169c0b49b45482e03f4
asdp80 <107992067+asdp80@users.noreply.github.com>
2024-07-26 19:26:17 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -1,13 +1,26 @@
+"""
+1. 높은 순 정렬
+2. 정렬한 것과 원래 emergency 비교해서 같은 값 인덱스 + 1 저장
+"""
+
 def solution(emergency):
-    # 응급도 리스트를 정렬하여 결과를 얻습니다.
-    sorted_emergency = sorted(emergency, reverse=True)
+    answer = sorted(emergency, reverse = True)
+    result = [] 
+    for i in emergency : 
+            result.append(answer.index(i) + 1)
     
-    # 순위를 저장할 리스트를 만듭니다.
-    answer = [0] * len(emergency)
-    
-    # 각 원소의 순위를 결정합니다.
-    for i, value in enumerate(emergency):
-        rank = sorted_emergency.index(value) + 1
-        answer[i] = rank
-    
-    return answer
+    return result
+"""
+def solution(emergency):
+    arr = []
+    for i in emergency:
+        idx = 1
+        for j in emergency:
+            if i < j:
+                idx += 1
+        arr.append(idx)
+    return arr
+"""
+
+
+
